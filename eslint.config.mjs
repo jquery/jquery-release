98cfd2fde1e6af9cd4753d529793bad038aa5234
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2025-09-23 22:58:22 +0200

    Build: Update deps, modernize CI, migrate the ESLint config
    Changes:
    1. Update dependencies
    2. Migrate to the ESLint flat config
    3. Test on Node.js 24, not 16
    4. Update actions
    5. Enable dependabot updates
    6. Fix lint issues

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import jqueryConfig from "eslint-config-jquery";
+import globals from "globals";
+
+export default [
+	{
+
+		// Only global ignores will bypass the parser
+		// and avoid JS parsing errors
+		// See https://github.com/eslint/eslint/discussions/17412
+		ignores: [
+			"__release/**"
+		]
+	},
+
+	jqueryConfig,
+
+	{
+		languageOptions: {
+			ecmaVersion: 2025,
+			sourceType: "script",
+
+			globals: {
+				...globals.node
+			}
+		},
+
+		rules: {
+			strict: [ "error", "global" ]
+		}
+	},
+
+	{
+		files: [ "*.mjs" ],
+
+		languageOptions: {
+			sourceType: "module"
+		}
+	}
+];
